644fd1493a3a40ba1356c9bb2401cb70b5a1b7ca
elisuh <elisaverna@gmail.com>
2016-09-11 18:36:53 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160910173956) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
